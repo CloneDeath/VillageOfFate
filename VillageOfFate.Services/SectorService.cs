1f13a8095c64704f7dd010d9d4f2a72542731167
Nicholas Rodine <halfofastaple@gmail.com>
2024-05-28 05:11:49 -0500

    Working on porting over the old code to services

--- VillageOfFate.Services/SectorService.cs
+++ VillageOfFate.Services/SectorService.cs
@@ -1,7 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using VillageOfFate.DAL;
 using VillageOfFate.DAL.Entities;
+using VillageOfFate.WebModels;
 
 namespace VillageOfFate.Services;
 
-public class SectorService {
-	public void CreateSector(SectorDto sector) { }
+public class SectorService(DataContext context) {
+	public async Task<SectorDto> GetSector(Position position) {
+		var sector = await context.Sectors.FirstOrDefaultAsync(s => s.X == position.X && s.Y == position.Y);
+		if (sector != null) {
+			return sector;
+		}
+
+		var result = await context.Sectors.AddAsync(new SectorDto {
+			Description =
+				"A dense, lush forest filled with towering trees, diverse wildlife, and the sounds of nature. " +
+				"It's easy to lose one's way in this vast sea of green.",
+			X = position.X,
+			Y = position.Y
+		});
+		await context.SaveChangesAsync();
+		return result.Entity;
+	}
 }
